b51294877aa74c9215d8b939491c492d824f0fe8
szzszzd <825923571@qq.com>
2026-09-05 14:03:03 +0800

    skip signing and notarizing the app.

--- packages/frontend/apps/electron/forge.config.mjs
+++ packages/frontend/apps/electron/forge.config.mjs
@@ -328,19 +328,19 @@ export default {
     name: productName,
     appBundleId: fromBuildIdentifier(appIdMap),
     icon: icnsPath,
-    osxSign: {
-      identity: 'Developer ID Application: TOEVERYTHING PTE. LTD.',
-      'hardened-runtime': true,
-    },
+    // osxSign: {
+    //   identity: 'Developer ID Application: TOEVERYTHING PTE. LTD.',
+    //   'hardened-runtime': true,
+    // },
     electronZipDir: process.env.ELECTRON_FORGE_ELECTRON_ZIP_DIR,
-    osxNotarize: process.env.APPLE_ID
-      ? {
-          tool: 'notarytool',
-          appleId: process.env.APPLE_ID,
-          appleIdPassword: process.env.APPLE_PASSWORD,
-          teamId: process.env.APPLE_TEAM_ID,
-        }
-      : undefined,
+    // osxNotarize: process.env.APPLE_ID
+    //   ? {
+    //       tool: 'notarytool',
+    //       appleId: process.env.APPLE_ID,
+    //       appleIdPassword: process.env.APPLE_PASSWORD,
+    //       teamId: process.env.APPLE_TEAM_ID,
+    //     }
+    //   : undefined,
     // We need the following line for updater
     extraResource: [
       './resources/app-update.yml',
